0a751e0ebdff488b4e2d9337add11d0a05c4769d
Luca Fernandez <luca.fernandez@spot.it>
2018-05-08 13:41:28 +0200

    prova banner 4

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express'),
 app = express();
 app.use(express.static('www'));
+app.get('*',function(req,res,next){
+    if(req.headers['x-forwarded-proto']!='https')
+      res.redirect('https://mypreferreddomain.com'+req.url)
+    else
+      next() /* Continue to other routes if we're not redirecting */
+  });
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
